49c9e73607f3a5a89a69bd6572d5739113a58be5
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2022-10-16 04:12:27 -0700

    socket stuff

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+CORS(app,resources={r"/*":{"origins":"*"}})
+# app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app,cors_allowed_origins="*")
 
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data)
 
+@socketio.on('ping')
+def handle_message(data):
+    """event listener when client types a message"""
+    print("data from the front end: ",str(data))
+    emit("data",{'data':data,'id':request.sid},broadcast=True)
+
+@socketio.on('connect')
+def test_connect():
+    print(request.sid)
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io("localhost:5000/", {
+  transports: ["websocket"],
+  cors: {
+    origin: "http://localhost:3000/",
+  },
+});
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+  const [socketInstance, setSocketInstance] = useState("");
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected')
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('pong', () => {
+      setLastPong(new Date().toISOString());
+    });
+    
+    setSocketInstance(socket);
+
+      socket.on("connect", (data) => {
+        console.log(data);
+      });
+
+      socket.on("disconnect", (data) => {
+        console.log(data);
+      });
+
+      return function cleanup() {
+        socket.disconnect();
+      };
+    // }
+
+    // return () => {
+    //   socket.off('connect');
+    //   socket.off('disconnect');
+    //   socket.off('pong');
+    // };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit('ping', {'test':123});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Connected: { '' + isConnected }</p>
+      <p>Last pong: { lastPong || '-' }</p>
+      <button onClick={ sendPing }>Send ping</button>
     </div>
   );
 }
