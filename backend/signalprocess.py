16a93ca4588fdd9c7c71e265256037405a8272e1
Shrey A <Shreystechtips@gmail.com>
2022-12-30 12:23:47 -0800

    done

--- backend/signalprocess.py
+++ backend/signalprocess.py
@@ -1,7 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.misc import electrocardiogram
 from scipy.signal import find_peaks
 import json
+
+with open('data.json','r') as f:
+    x = np.array(json.load(f))
+# x = electrocardiogram()[2000:4000]
+
+peaks, _ = find_peaks(x, height=0)
+plt.plot(x)
+plt.plot(peaks, x[peaks], "x")
+plt.plot(np.zeros_like(x), "--", color="gray")
+plt.show()
+
 
 def peak_calc(datas, lookback = 2, combine_dist=50, cutoff = 50):
     """calculate the number of repeats in gesture detected
